8a0a960627eb629c72caea6ce72c1f49e0fa5289
Tim Boldt <tim.boldt@gmail.com>
2021-03-11 22:50:38 -0800

    Switch to LCD display

--- feather-m0-clock/main.go
+++ feather-m0-clock/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"image/color"
 	"machine"
 
 	"time"
 
 	"tinygo.org/x/drivers/ds3231"
-	"tinygo.org/x/drivers/ssd1306"
+	"tinygo.org/x/drivers/hd44780i2c"
 )
 
 func main() {
@@ -26,24 +25,19 @@ func main() {
 	clock := ds3231.New(machine.I2C0)
 	clock.Configure()
 
-	// Initialize display.
-	display := ssd1306.NewI2C(machine.I2C0)
-	display.Configure(ssd1306.Config{
-		Address: ssd1306.Address_128_32,
-		Width:   128,
-		Height:  32,
-	})
+	lcd := hd44780i2c.New(machine.I2C0, 0x27) // some modules have address 0x3F
 
-	display.ClearDisplay()
+	lcd.Configure(hd44780i2c.Config{
+		Width:       20, // required
+		Height:      4,  // required
+		CursorOn:    false,
+		CursorBlink: false,
+	})
 
 	// Initialize Battery Voltage ADC.
 	vbat := machine.ADC{Pin: machine.D9}
 	vbat.Configure(machine.ADCConfig{})
 
-	x := int16(0)
-	y := int16(0)
-	deltaX := int16(1)
-	deltaY := int16(1)
 	for {
 		//
 		// === Read the sensors ===
@@ -68,24 +62,10 @@ func main() {
 			batteryMilliVolts/1000, batteryMilliVolts%1000/10)
 		println(clockTime.Format(time.Kitchen), statusInfo)
 
-		pixel := display.GetPixel(x, y)
-		c := color.RGBA{255, 255, 255, 255}
-		if pixel {
-			c = color.RGBA{0, 0, 0, 255}
-		}
-		display.SetPixel(x, y, c)
-		display.Display()
-
-		x += deltaX
-		y += deltaY
-
-		if x == 0 || x == 127 {
-			deltaX = -deltaX
-		}
-
-		if y == 0 || y == 31 {
-			deltaY = -deltaY
-		}
+		lcd.ClearDisplay()
+		lcd.Print([]byte(statusInfo))
+		lcd.Print([]byte("\n\n"))
+		lcd.Print([]byte(clockTime.Format(time.ANSIC)))
 
 		time.Sleep(1 * time.Second)
 	}
